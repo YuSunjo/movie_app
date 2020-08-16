c07adbdebe1423bb059553543a1c896d4636e3b7
YuSunjo <tnswh2023@gmail.com>
2020-08-16 10:39:58 +0900

    Updates

---
+++ precache-manifest.c85ba03ca9a14f00203ce906e7a48590.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d860ff7b3394e4a53fb16d8b29e747",
+    "url": "/YuSunjo/movie_app/index.html"
+  },
+  {
+    "revision": "9994ddd0aa0424bf2ec2",
+    "url": "/YuSunjo/movie_app/static/css/main.07e28d20.chunk.css"
+  },
+  {
+    "revision": "109356d05735fcb9e967",
+    "url": "/YuSunjo/movie_app/static/js/2.670a5eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/YuSunjo/movie_app/static/js/2.670a5eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9994ddd0aa0424bf2ec2",
+    "url": "/YuSunjo/movie_app/static/js/main.2fd5edad.chunk.js"
+  },
+  {
+    "revision": "4eb55ed5e8eff8ab1148",
+    "url": "/YuSunjo/movie_app/static/js/runtime-main.a25e07b9.js"
+  }
+]);
